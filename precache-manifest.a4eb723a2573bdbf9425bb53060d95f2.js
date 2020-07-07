8f9dc1f46588245920d45fc6feca8571f0ed32ce
fabnihl <32229462+fabnihl@users.noreply.github.com>
2020-07-07 18:02:06 +0100

    Updates

---
+++ precache-manifest.a4eb723a2573bdbf9425bb53060d95f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e42a54451bfbd1dac13c1872deceb28c",
+    "url": "/react-project/index.html"
+  },
+  {
+    "revision": "8eab395ca512f38c1b6a",
+    "url": "/react-project/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "57d9c48641e3c9c23766",
+    "url": "/react-project/static/js/2.e3de18de.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-project/static/js/2.e3de18de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8eab395ca512f38c1b6a",
+    "url": "/react-project/static/js/main.cb6b1a85.chunk.js"
+  },
+  {
+    "revision": "bb90f854d6c9ae26f9a2",
+    "url": "/react-project/static/js/runtime-main.abb713e9.js"
+  }
+]);
